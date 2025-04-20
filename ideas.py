d118c2c44c5bfe26ef16e1c1888c725ba163aa26
Dallss <126046337+Dallss@users.noreply.github.com>
2025-04-21 02:45:15 +0800

    Create ideas.py

---
+++ ideas.py
@@ -0,0 +1,9 @@
+- ideas.
+
+University-wide insights
+
+    # accepts titles of all researches in the univ
+  - def univeristyResearchAreasPartitions():
+      # supposed to return a partition of towards what area
+      # has the university been focusing on in research
+
