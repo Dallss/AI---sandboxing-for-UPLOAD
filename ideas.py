680b5ff33290d398385705bf2ab294a29e3dd754
Dallss <126046337+Dallss@users.noreply.github.com>
2025-04-21 02:51:39 +0800

    Update ideas.py

--- ideas.py
+++ ideas.py
@@ -7,3 +7,7 @@ University-wide insights
       # supposed to return a partition of towards what area
       # has the university been focusing on in research
 
+
+  - def semanticSearch():
+    # a smarter way to search. one that's context aware.
+
